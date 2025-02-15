9d84b25579d6d84082f2bc537b7b9bea77228fa5
aiduma <aiduma08@gmail.com>
2025-02-14 18:01:58 -0700

    Added Changes for staging

---
+++ azure-app-aiduma-vs/Pages/DeploymentSlots.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace azure_app_aiduma_vs.Pages
+{
+    public class DeploymentSlotsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
